5e97c50083843700d4d21d908ffe820ac80c02d9
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-01-31 11:31:11 +0530

    Create ASCII ART.py

---
+++ ASCII ART.py
@@ -0,0 +1,468 @@
+rows = 55
+cols = 100
+for i in range(rows):
+    for j in range(cols):
+        if i==0 and 0<=j<100:
+            print("@",end="")
+        elif i==1 and 0<=j<100:
+            print("@",end="")
+        elif i==2 and 0<=j<43:
+            print("@",end="")
+        elif i==2 and j==43:
+            print("=",end="")
+        elif i==2 and 44<=j<=45:
+            print("#",end="")
+        elif i==2 and 46<=j<=48:
+            print("%",end="")
+        elif i==2 and 49<=j<=51:
+            print("@",end="")
+        elif i==2 and j==52:
+            print("%",end="")
+        elif i==2 and j==53:
+            print("#",end="")
+        elif i==2 and 54<=j<100:
+            print("@",end="")
+        elif i==3 and 0<=j<=39:
+            print("@",end="")
+        elif i==3 and j==40:
+            print("#",end="")
+        elif i==3 and 41<=j<=48:
+            print("%",end="")
+        elif i==3 and 49<=j<=55:
+            print("@",end="")
+        elif i==3 and j==56:
+            print("%",end="")
+        elif i==3 and 57<=j<=100:
+            print("@",end="")
+        elif i==4 and 0<=j<=37:
+            print("@",end="")
+        elif i==4 and j==38:
+            print("%",end="")
+        elif i==4 and 39<=j<=56:
+            print("@",end="")
+        elif i==4 and j==57:
+            print("%",end="")
+        elif i==4 and j==58:
+            print("#",end="")
+        elif i==4 and j==59:
+            print("*",end="")
+        elif i==4 and 60<=j<100:
+            print("@",end="")
+        elif i==5 and 0<=j<=37:
+            print("@",end="")
+        elif i==5 and j==38:
+            print("%",end="")
+        elif i==5 and 39<=j<=58:
+            print("@",end="")
+        elif i==5 and j==59:
+            print("%",end="")
+        elif i==5 and j==60:
+            print("#",end="")
+        elif i==5 and 61<=j<100:
+            print("@",end="")
+        elif i==6 and 0<=j<=62:
+            print("@",end="")
+        elif i==6 and j==63:
+            print("%",end="")
+        elif i==6 and 64<=j<100:
+            print("@",end="")
+        elif i==7 and 0<=j<=36:
+            print("@",end="")
+        elif i==7 and j==37:
+            print("#",end="")
+        elif i==7 and 38<=j<=41:
+            print("@",end="")
+        elif i==7 and j==42:
+            print("#",end="")
+        elif i==7 and j==43:
+            print("+",end="")
+        elif i==7 and 44<=j<=47:
+            print("=",end="")
+        elif i==7 and j==48:
+            print("-",end="")
+        elif i==7 and 49<=j<=51:
+            print(":",end="")
+        elif i==7 and 52<=j<=54:
+            print("-",end="")
+        elif i==7 and 55<=j<=56:
+            print(":",end="")
+        elif i==7 and j==57:
+            print("+",end="")
+        elif i==7 and j==58:
+            print("%",end="")
+        elif i==7 and 59<=j<=61:
+            print("@",end="")
+        elif i==7 and j==62:
+            print("%",end="")
+        elif i==7 and 63<=j<100:
+            print("@",end="")
+        elif i==8 and 0<=j<=36:
+            print("@",end="")
+        elif i==8 and j==37:
+            print("%",end="")
+        elif i==8 and 38<=j<=40:
+            print("@",end="")
+        elif i==8 and j==41:
+            print("*",end="")
+        elif i==8 and j==42:
+            print("=",end="")
+        elif i==8 and j==43:
+            print("-",end="")
+        elif i==8 and j==44:
+            print(":",end="")
+        elif i==8 and 45<=j<=47:
+            print(".",end="")
+        elif i==8 and 48<=j<=52:
+            print(" ",end="")
+        elif i==8 and 53<=j<=56:
+            print(".",end="")
+        elif i==8 and j==57:
+            print(":",end="")
+        elif i==8 and j==58:
+            print("=",end="")
+        elif i==8 and j==59:
+            print("%",end="")
+        elif i==8 and 60<=j<=62:
+            print("@",end="")
+        elif i==8 and j==63:
+            print("=",end="")
+        elif i==8 and 64<=j<100:
+            print("@",end="")
+        elif i==9 and 0<=j<=36:
+            print("@",end="")
+        elif i==9 and j==37:
+            print("%",end="")
+        elif i==9 and 38<=j<=39:
+            print("@",end="")
+        elif i==9 and j==40:
+            print("#",end="")
+        elif i==9 and 41<=j<=42:
+            print("=",end="")
+        elif i==9 and j==43:
+            print("-",end="")
+        elif i==9 and 44<=j<=45:
+            print(":",end="")
+        elif i==9 and 46<=j<=48:
+            print(".",end="")
+        elif i==9 and 49<=j<=50:
+            print(" ",end="")
+        elif i==9 and 51<=j<=56:
+            print(".",end="")
+        elif i==9 and 57<=j<=58:
+            print(":",end="")
+        elif i==9 and j==59:
+            print("*",end="")
+        elif i==9 and 60<=j<=61:
+            print("@",end="")
+        elif i==9 and j==62:
+            print("%",end="")
+        elif i==9 and j==63:
+            print(":",end="")
+        elif i==9 and 64<=j<100:
+            print("@",end="")
+        elif i==10 and 0<=j<=39:
+            print("@",end="")
+        elif i==10 and j==40:
+            print("%",end="")
+        elif i==10 and 41<=j<=42:
+            print("=",end="")
+        elif i==10 and j==43:
+            print("-",end="")
+        elif i==10 and 44<=j<=45:
+            print(":",end="")
+        elif i==10 and 46<=j<=55:
+            print(".",end="")
+        elif i==10 and 56<=j<=58:
+            print(":",end="")
+        elif i==10 and j==59:
+            print("+",end="")
+        elif i==10 and 60<=j<=61:
+            print("@",end="")
+        elif i==10 and j==62:
+            print("%",end="")
+        elif i==10 and 63<=j<100:
+            print("@",end="")
+        elif i==11 and 0<=j<=39:
+            print("@",end="")
+        elif i==11 and j==40:
+            print("+",end="")
+        elif i==11 and j==41:
+            print("=",end="")
+        elif i==11 and 42<=j<=46:
+            print("+",end="")
+        elif i==11 and j==47:
+            print("=",end="")
+        elif i==11 and j==48:
+            print("-",end="")
+        elif i==11 and 49<=j<=50:
+            print(":",end="")
+        elif i==11 and j==51:
+            print("-",end="")
+        elif i==11 and j==52:
+            print("=",end="")
+        elif i==11 and 53<=j<=54:
+            print("+",end="")
+        elif i==11 and 55<=j<=56:
+            print("=",end="")
+        elif i==11 and j==57:
+            print("-",end="")
+        elif i==11 and 58<=j<=59:
+            print(":",end="")
+        elif i==11 and j==60:
+            print("#",end="")
+        elif i==11 and j==61:
+            print("@",end="")
+        elif i==11 and j==62:
+            print("#",end="")
+        elif i==11 and 63<=j<100:
+            print("@",end="")
+        elif i==12 and 0<=j<=37:
+            print("@",end="")
+        elif i==12 and j==38:
+            print("+",end="")
+        elif i==12 and j==39:
+            print("%",end="")
+        elif i==12 and 40<=j<=41:
+            print("+",end="") 
+        elif i==12 and 42<=j<=44:
+            print("#",end="")
+        elif i==12 and j==45:
+            print("%",end="")
+        elif i==12 and j==46:
+            print("#",end="")
+        elif i==12 and j==47:
+            print("%",end="")
+        elif i==12 and j==48:
+            print("*",end="")
+        elif i==12 and 49<=j<=50:
+            print(":",end="")
+        elif i==12 and j==51:
+            print("+",end="")
+        elif i==12 and 52<=j<=54:
+            print("#",end="")
+        elif i==12 and j==55:
+            print("*",end="")
+        elif i==12 and 56<=j<=57:
+            print("+",end="")
+        elif i==12 and j==58:
+            print("-",end="")
+        elif i==12 and j==59:
+            print(":",end="")
+        elif i==12 and j==60:
+            print("=",end="")
+        elif i==12 and j==61:
+            print("#",end="")
+        elif i==12 and j==62:
+            print(":",end="")
+        elif i==12 and 63<=j<100:
+            print("@",end="")
+        elif i==13 and 0<=j<=36:
+            print("@",end="")
+        elif i==13 and j==37:
+            print("+",end="")
+        elif i==13 and 38<=j<=39:
+            print("*",end="")
+        elif i==13 and j==40:
+            print("=",end="")
+        elif i==13 and j==41:
+            print("-",end="")
+        elif i==13 and j==42:
+            print("+",end="")
+        elif i==13 and 43<=j<=44:
+            print("*",end="")
+        elif i==13 and j==45:
+            print("+",end="")
+        elif i==13 and 46<=j<=47:
+            print("=",end="")
+        elif i==13 and j==48:
+            print("-",end="")
+        elif i==13 and j==49:
+            print(":",end="")
+        elif i==13 and j==50:
+            print(".",end="")
+        elif i==13 and j==51:
+            print(":",end="")
+        elif i==13 and j==52:
+            print("-",end="")
+        elif i==13 and 53<=j<=56:
+            print("=",end="")
+        elif i==13 and j==57:
+            print("-",end="")
+        elif i==13 and j==58:
+            print(":",end="")
+        elif i==13 and j==59:
+            print(".",end="")
+        elif i==13 and 60<=j<=61:
+            print("-",end="")
+        elif i==13 and j==62:
+            print("=",end="")
+        elif i==13 and j==63:
+            print(" ",end="")
+        elif i==13 and 64<=j<100:
+            print("@",end="")
+        elif i==14 and 0<=j<=36:
+            print("@",end="")
+        elif i==14 and j==37:
+            print("=",end="")
+        elif i==14 and 38<=j<=39:
+            print("+",end="")
+        elif i==14 and 40<=j<=41:
+            print("-",end="")
+        elif i==14 and 42<=j<=44:
+            print(":",end="")
+        elif i==14 and 45<=j<=46:
+            print(".",end="")
+        elif i==14 and j==47:
+            print("-",end="")
+        elif i==14 and j==48:
+            print(":",end="")
+        elif i==14 and 49<=j<=58:
+            print(".",end="")
+        elif i==14 and j==59:
+            print(":",end="")
+        elif i==14 and j==60:
+            print("-",end="")
+        elif i==14 and j==61:
+            print(":",end="")
+        elif i==14 and j==62:
+            print(" ",end="")
+        elif i==14 and 63<=j<100:
+            print("@",end="")
+        elif i==15 and 0<=j<=37:
+            print("@",end="")
+        elif i==15 and j==38:
+            print("=",end="")
+        elif i==15 and j==39:
+            print("+",end="")
+        elif i==15 and j==40:
+            print("=",end="")
+        elif i==15 and 41<=j<=42:
+            print("-",end="")
+        elif i==15 and j==43:
+            print(":",end="")
+        elif i==15 and 44<=j<=45:
+            print(".",end="")
+        elif i==15 and j==46:
+            print(":",end="")
+        elif i==15 and 47<=j<=48:
+            print("=",end="")
+        elif i==15 and j==49:
+            print("-",end="")
+        elif i==15 and 50<=j<=53:
+            print(":",end="")
+        elif i==15 and 54<=j<=59:
+            print(".",end="")
+        elif i==15 and 60<=j<=61:
+            print("-",end="")
+        elif i==15 and j==62:
+            print(".",end="")
+        elif i==15 and 63<=j<100:
+            print("@",end="")
+        elif i==16 and 0<=j<=37:
+            print("@",end="")
+        elif i==16 and 38<=j<=39:
+            print("*",end="")
+        elif i==16 and 40<=j<=41:
+            print("=",end="")
+        elif i==16 and 42<=j<=43:
+            print("-",end="")
+        elif i==16 and 44<=j<=46:
+            print(":",end="")
+        elif i==16 and j==47:
+            print("*",end="")
+        elif i==16 and 48<=j<=49:
+            print("@",end="")
+        elif i==16 and j==50:
+            print("%",end="")
+        elif i==16 and j==51:
+            print("#",end="")
+        elif i==16 and j==52:
+            print("+",end="")
+        elif i==16 and 53<=j<=59:
+            print(":",end="")
+        elif i==16 and j==60:
+            print("+",end="")
+        elif i==16 and j==61:
+            print("=",end="")
+        elif i==16 and j==62:
+            print(" ",end="")
+        elif i==16 and 63<=j<100:
+            print("@",end="")
+        elif i==17 and 0<=j<=37:
+            print("@",end="")
+        elif i==17 and j==38:
+            print("=",end="")
+        elif i==17 and j==39:
+            print("#",end="")
+        elif i==17 and j==40:
+            print("*",end="")
+        elif i==17 and j==41:
+            print("+",end="")
+        elif i==17 and 42<=j<=44:
+            print("=",end="")
+        elif i==17 and j==45:
+            print("+",end="")
+        elif i==17 and j==46:
+            print("*",end="")
+        elif i==17 and 47<=j<=50:
+            print("#",end="")
+        elif i==17 and j==51:
+            print("*",end="")
+        elif i==17 and j==52:
+            print("#",end="")
+        elif i==17 and j==53:
+            print("*",end="")
+        elif i==17 and j==54:
+            print("+",end="")
+        elif i==17 and j==55:
+            print("-",end="")
+        elif i==17 and 56<=j<=57:
+            print(":",end="")
+        elif i==17 and j==58:
+            print("=",end="")
+        elif i==17 and j==59:
+            print("+",end="")
+        elif i==17 and j==60:
+            print("*",end="")
+        elif i==17 and j==61:
+            print("-",end="")
+        elif i==17 and j==62:
+            print(" ",end="")
+        elif i==17 and 63<=j<100:
+            print("@",end="")
+        elif i==18 and 0<=j<=38:
+            print("@",end="")
+        elif i==18 and j==39:
+            print("#",end="")
+        elif i==18 and j==40:
+            print("%",end="")
+        elif i==18 and 41<=j<=42:
+            print("#",end="")
+        elif i==18 and 43<=j<=44:
+            print("*",end="")
+        elif i==18 and 45<=j<=46:
+            print("%",end="")
+        elif i==18 and j==47:
+            print("=",end="")
+        elif i==18 and 48<=j<=52:
+            print("=",end="")
+        elif i==18 and j==53:
+            print("*",end="")
+        elif i==18 and j==54:
+            print("%",end="")
+        elif i==18 and j==55:
+            print("#",end="")
+        elif i==18 and j==56:
+            print("=",end="")
+        elif i==18 and j==57:
+            print("+",end="")
+        elif i==18 and 58<=j<=59:
+            print("*",end="")
+        elif i==18 and j==60:
+            print("#",end="")
+        elif i==18 and j==61:
+            print("=",end="")
+        elif i==18 and 62<=j<100:
+            print("@",end="")
+        
+    print()
